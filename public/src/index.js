4311188d2b98e63f368ef16319c03887b6cb0c54
stimorol95 <artem2695post@yandex.ru>
2022-07-05 21:50:38 +0300

    homework_3

--- public/src/index.js
+++ public/src/index.js
@@ -1,17 +1,28 @@
-import { renderSearchFormBlock } from './search-form.js';
-import { renderSearchStubBlock } from './search-results.js';
-import { renderUserBlock } from './user.js';
-import { renderToast } from './lib.js';
-const userName = 'Wade Warren';
-const avatarSrc = 'img/avatar.png';
-const favoriteItemsAmount = 0;
-const checkInDate = new Date();
-checkInDate.setDate(checkInDate.getDate() + 1);
-const checkOutDate = new Date();
-checkOutDate.setDate(checkInDate.getDate() + 2);
-window.addEventListener('DOMContentLoaded', () => {
-    renderUserBlock(userName, avatarSrc, favoriteItemsAmount);
-    renderSearchFormBlock(checkInDate, checkOutDate);
+import { renderSearchFormBlock } from "./search-form.js";
+import { renderSearchStubBlock } from "./search-results.js";
+import { renderUserBlock } from "./user.js";
+import { renderToast } from "./lib.js";
+import { searchHandler } from "./search-form-data.js";
+import { getTodosByCount } from "./api.js";
+localStorage.setItem("user", "");
+localStorage.setItem("favoritesAmount", "6");
+localStorage.user = JSON.stringify({
+    userName: "Girl",
+    userAvatar: "/img/avatar.png",
+});
+function getUserData() {
+    let user = JSON.parse(localStorage.getItem("user"));
+    return user;
+}
+function getFavoritesAmount() {
+    let favorite = localStorage.getItem("favoritesAmount");
+    return favorite;
+}
+window.addEventListener("DOMContentLoaded", () => {
+    renderUserBlock(getUserData().userName, getUserData().userAvatar, getFavoritesAmount());
+    renderSearchFormBlock();
     renderSearchStubBlock();
+    searchHandler();
+    getTodosByCount(3);
     renderToast({ text: 'Это пример уведомления. Используйте его при необходимости', type: 'success' }, { name: 'Понял', handler: () => { console.log('Уведомление закрыто'); } });
 });
